b3c7c50c718bf6b9202a18e8d9126961c6f8ee09
Ginzo974 <quentin.louis97400@gmail.com>
2022-08-03 22:01:20 +0400

    modification bouton

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-end m-2 p-2">
                 <a href="{{ route('admin.reservations.create') }}"
-                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Gérer les réservations</a>
+                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Ajouter</a>
             </div>
             <div class="flex flex-col">
                 <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
